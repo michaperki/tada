5a238d4fa0ef24325e960b68a823bcb2459d9503
mperkins1995 <mperkins1995@gmail.com>
2023-02-22 18:22:31 -0600

    completed tutorial p3

--- mysite/tada/urls.py
+++ mysite/tada/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tada'
 urlpatterns = [
+    # ex: /tada/
     path('', views.index, name='index'),
+    # ex: /tada/5/
+    path('<int:tada_id>/', views.detail, name='detail'),
+    # ex: /tada/5/results/
+    path('<int:tada_id>/results/', views.results, name='results'),
 ]
--- mysite/tada/views.py
+++ mysite/tada/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Tada
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Tada index.")
+    latest_list = Tada.objects.order_by('-pub_date')[:5]
+    context = {'latest_list': latest_list}
+    return render(request, 'tada/index.html', context)
+
+def detail(request, tada_id):
+    tada = get_object_or_404(Tada, pk=tada_id)
+    return render(request, 'tada/detail.html', {'tada': tada})
+
+def results(request, tada_id):
+    response = "You're looking at the results of tada %s."
+    return HttpResponse(response % tada_id)
