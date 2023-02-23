659c06900acae7d6a4704b4c256cda1a164dd80c
mperkins1995 <mperkins1995@gmail.com>
2023-02-22 18:31:31 -0600

    added Module model

--- mysite/tada/models.py
+++ mysite/tada/models.py
@@ -13,3 +13,15 @@ class Tada(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Module(models.Model):
+    tada = models.ForeignKey(Tada, on_delete=models.CASCADE)
+    module_name = models.CharField(max_length=200)
+    module_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.module_name
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
